a66af64ca7f3d94c4a1192f00e9e31931286c6d7
Tony Nguyen <tonynguyeenn@gmail.com>
2023-10-21 15:29:08 -0700

    Preparing MongoDB and mongoose for CRUD operations

---
+++ models/workout.js
@@ -0,0 +1,15 @@
+import {Schema, mongoose} from "mongoose";
+
+const workoutSchema = new Schema(
+    {
+        title: String,
+        description: String,
+    },
+    {
+        timestamps: true,
+    }
+)
+
+const Workout = mongoose.models.Workout || mongoose.model("Workout", workoutSchema)
+
+export default Workout
