ac518e62e2103044a4eba39d5a7c5bb058744b2e
Tony Nguyen <tonynguyeenn@gmail.com>
2023-12-25 11:53:59 -0800

    Implementing way to display user workouts

--- components/UserInfo.jsx
+++ components/UserInfo.jsx
@@ -8,6 +8,25 @@ export default function UserInfo() {
 
   const  {data:session} = useSession();
 
+  const getWorkoutById = async (id) => {
+    try {
+      const res = await fetch(`http://localhost:3000/api/workouts/${id}`, {
+        cache: "no-store",
+      });
+  
+      if (!res.ok) {
+        throw new Error("Failed to fetch workout");
+      }
+  
+      return res.json();
+    } catch (error) {
+      console.log("Error loading workout: ", error);
+    }
+  };
+
+  await connectMongoDB();
+  const user = await User.findOne({email});
+
   return (
     <div>
       <div>Name: {session?.user?.name}</div>
